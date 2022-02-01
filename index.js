43bcd7f285a766de65d27769cd76c5aca31c27c4
Michael Farias <mfarias496@gmail.com>
2022-01-31 21:43:40 -0500

    Updated portfolio Feb 2022

--- index.js
+++
@@ -1,8 +0,0 @@
-function flipCard(cardID) {
-	let card = document.querySelector(`#${cardID}`);
-	if (card.className === "flip-card") {
-		card.className = "flip-card-flipped";
-	} else {
-		card.className = "flip-card";
-	}
-}
